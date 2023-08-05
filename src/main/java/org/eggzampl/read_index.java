00bae590b0f1720b6c5ea89f8a9e2d31820a2a51
reyan1357 <singhhimangshu20@gmail.com>
2023-08-05 10:14:18 +0530

    added json output and parent folder identification

--- src/main/java/org/eggzampl/read_index.java
+++ src/main/java/org/eggzampl/read_index.java
@@ -1,5 +1,9 @@
 package org.eggzampl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
@@ -11,6 +15,11 @@ import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import java.io.File;
+import java.lang.reflect.Type;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
 import java.nio.file.attribute.UserPrincipal;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,7 +31,13 @@ import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class read_index {
+
     static void Main(String searchTerm, String INDEX_DIR) throws IOException, ParseException {
+
+        File pfile = new File(INDEX_DIR);
+//        System.out.println("File: " + pfile);
+//        System.out.println("Parent: " + pfile.getParent());
+
 
         // Field to search on
         String fieldName = "contents";
@@ -30,36 +45,88 @@ public class read_index {
         List<Document> matchingDocuments = performExactMatchQuery(INDEX_DIR, fieldName, searchTerm);
 
         System.out.println("Search Term: " + searchTerm);
-//        System.out.println("Field Name: " + fieldName);
         System.out.println("Number of Hits: " + matchingDocuments.size());
+        System.out.println("Results: \n");
+
+
+        // Specify the file path where you want to save the JSON output
+        String filePath = pfile.getParent();
+        deleteFile(getOutputFilePath(filePath));                                //this gets the parent directory of current specified directory
+
+        List<UserData> existingData = readExistingData(getOutputFilePath(filePath));
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         // Process and display the matching documents
         for (Document doc : matchingDocuments) {
-            System.out.println("\n\nDocument Path:      " + doc.get("path"));// + ", Content: "+ doc.get(fieldName));
-
-            {
-                try {
-//  Access time snippet
-                    Path file = Paths.get(doc.get("path"));
-                    BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-                    System.out.println("creationTime:       " + attr.creationTime());
+            System.out.println("\n\nDocument Path:      " + doc.get("path"));
+            try {
+                //  Access time snippet
+                Path file = Paths.get(doc.get("path"));
+                BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+                System.out.println("creationTime:       " + attr.creationTime());
+                String time = attr.creationTime().toString();
 //                    System.out.println("lastAccessTime:     " + attr.lastAccessTime());
 //                    System.out.println("lastModifiedTime:   " + attr.lastModifiedTime());
 
-//  Owner Name Snippet
-                    FileOwnerAttributeView path = Files.getFileAttributeView(file, FileOwnerAttributeView.class);
+                FileOwnerAttributeView path = Files.getFileAttributeView(file, FileOwnerAttributeView.class);
+                UserPrincipal user = path.getOwner();                           // Taking owner name from the file
+                System.out.println("Owner:              " + user.getName());    // Printing the owner's name
 
-                    UserPrincipal user = path.getOwner();                           // Taking owner name from the file
-                    System.out.println("Owner:              " + user.getName());    // Printing the owner's name
+                UserData userData = new UserData(doc.get("path"),time,user.getName());
+                existingData.add(userData);
 
-                } catch (IOException e) {
-                    System.out.println("Error while Displaying Time !");
-                }
+            } catch (IOException e) {
+                System.out.println("Error while Displaying Time !");
             }
         }
+//-----------------------------------------------
+        try {
+            // Convert the updated data list to a JSON string
+            String jsonString = gson.toJson(existingData);
+
+            // Write the JSON string to the file
+            FileWriter fileWriter = new FileWriter(getOutputFilePath(filePath));
+            fileWriter.write(jsonString);
+            fileWriter.flush();
+            fileWriter.close();
+
+            System.out.println("JSON output has been written to " + getOutputFilePath(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//-----------------------------------------------
     }
 
-    private static List<Document> performExactMatchQuery(String INDEX_DIR, String fieldName, String searchTerm) throws IOException, ParseException {
+    private static String getOutputFilePath(String inputFilePath) {
+        File inputFile = new File(inputFilePath);
+        String parentDir = inputFile.getParent();
+        return parentDir + File.separator + "Search_Result.json";
+    }
+    private static void deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+    private static List<UserData> readExistingData(String filePath) {
+        List<UserData> existingData = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            Type dataType = new TypeToken<List<UserData>>() {}.getType();
+            Gson gson = new Gson();
+            existingData = gson.fromJson(fileReader, dataType);
+            fileReader.close();
+        } catch (IOException e) {
+            // If the file doesn't exist or cannot be read, ignore the exception and return an empty list
+        }
+
+        return existingData;
+    }
+
+
+
+        private static List<Document> performExactMatchQuery(String INDEX_DIR, String fieldName, String searchTerm) throws IOException, ParseException {
         List<Document> matchingDocuments = new ArrayList<>();
 
         // Open the index directory
@@ -72,27 +139,46 @@ public class read_index {
         QueryParser queryParser = new QueryParser(fieldName, analyzer);
 
         // Create an exact match query for the search term
-        Query query = queryParser.parse('"' + searchTerm + '"');
+        Query query = queryParser.parse('"'+searchTerm+'"');
 
         // Perform the search and retrieve matching documents
         int maxHits = 10; // Maximum Number of search results to retrieve
         TopDocs topDocs = searcher.search(query, maxHits);
+
+        Path indexDirPath = Paths.get(INDEX_DIR).toRealPath(); // change
         ScoreDoc[] hits = topDocs.scoreDocs;
         for (ScoreDoc hit : hits) {
             int docId = hit.doc;
             Document document = searcher.doc(docId);
-            matchingDocuments.add(document);
+            String documentPath = document.get("path");
+            Path documentRealPath;
+            try {
+                documentRealPath = Paths.get(documentPath).toRealPath();
+            } catch (IOException e) {
+                // If there's an error with the file path, continue to the next document
+                continue;
+            }
+            if (!documentRealPath.startsWith(indexDirPath)) {
+                matchingDocuments.add(document);
+            }
         }
-
-        // matchingdocument contains all the content of the matched file
-
-//        System.out.println("Printing MATCHINGDOCUMENTS \n\n"+matchingDocuments+"\n\nPRINTED MATCHING DOCUMENTS");
 
         // Close the index reader and directory
         indexReader.close();
         directory.close();
 
         return matchingDocuments;
+    }
+
+    private static class UserData {
+        String Path;
+        String Creation_Time;
+        String Owner;
+        public UserData(String Path, String Creation_Time, String Owner) {
+            this.Path = Path;
+            this.Creation_Time = Creation_Time;
+            this.Owner = Owner;
+        }
     }
 }
 
--- src/main/java/org/eggzampl/write_index.java
+++ src/main/java/org/eggzampl/write_index.java
@@ -29,15 +29,18 @@ import static java.lang.System.exit;
 
 public class write_index
 {
-    public static String folderExistsInDirectory(String docsPath, String childFolder) {
-        File folder = new File(docsPath, childFolder);
+
+    private static final String INDEX_FOLDER = "indexedFiles";                 //name of the folder where the indexed files will be stored
+
+    public static String folderExistsInDirectory(String docsPath, String INDEX_FOLDER) {
+        File folder = new File(docsPath, INDEX_FOLDER);
         if(folder.exists() && folder.isDirectory())
             return folder.getAbsolutePath();
         return null;
     }
 
-    public static String createFolderInDirectory(String docsPath, String childFolder) {
-        File folder = new File(docsPath, childFolder);
+    public static String createFolderInDirectory(String docsPath, String INDEX_FOLDER) {
+        File folder = new File(docsPath, INDEX_FOLDER);
         if(folder.mkdir())
             return folder.getAbsolutePath();
         return null;
@@ -46,19 +49,17 @@ public class write_index
 
     static void Main(String docsPath)
     {
-        String childFolder = "indexedFiles";                 //name of the folder where the indexed files will be stored
-
         final Path docDir = Paths.get(docsPath);
 
         File parentDir = new File(docsPath);
-        File childDir = new File(parentDir, childFolder);
+        File childDir = new File(parentDir, INDEX_FOLDER);
 
-        String pathh = folderExistsInDirectory(docsPath, childFolder);
+        String pathh = folderExistsInDirectory(docsPath, INDEX_FOLDER);
         if (pathh != null) {
             System.out.println("The folder exists within the parent directory.");
             System.out.println("Path: " + pathh);
         } else {
-            String indexdir = createFolderInDirectory(docsPath, childFolder);
+            String indexdir = createFolderInDirectory(docsPath, INDEX_FOLDER);
             if (indexdir != null) {
                 System.out.println("The folder was successfully created in the parent directory.");
                 System.out.println("Path: " + indexdir);
@@ -66,6 +67,8 @@ public class write_index
                 System.out.println("Failed to create the folder in the parent directory.");
             }
         }
+        String indexFolderAbsolutePath = Paths.get(docsPath, INDEX_FOLDER).toAbsolutePath().toString();
+
 
         String indexPath = childDir.getPath();                      //getting the path of the indexed Folder
 
@@ -98,7 +101,7 @@ public class write_index
     static void indexDocs(final IndexWriter writer, Path path) throws IOException
     {
         //if directory exists then continue
-        if (Files.isDirectory(path))
+        if (Files.isDirectory(path) && !path.endsWith(INDEX_FOLDER))
         {
             //Iterate directory
             Files.walkFileTree(path, new SimpleFileVisitor<Path>()
@@ -119,15 +122,19 @@ public class write_index
                 }
             });
         }
-        else
-        {
-            //Index this file
+        else if (!Files.isDirectory(path)) {
+            // Index this file if it's not a directory
             indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
         }
     }
 
     static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException
     {
+        if (!Files.isRegularFile(file) || !Files.isReadable(file) || !file.toString().endsWith(".txt")) {
+            // Skip non-text files or files that cannot be read or not ending with ".txt"
+            return;
+        }
+
         try (InputStream stream = Files.newInputStream(file))
         {
             //Create lucene Document
